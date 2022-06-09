b6f2ca4d03494bd44464fd2bcf76083d1ec25c22
Nikhil Tomar <nikhiltomar2690@gmail.com>
2022-06-09 14:17:59 +0530

    Learn Js

---
+++ array.js
@@ -0,0 +1,54 @@
+let arr = ['India','Sri Lanka','United States','Canada','Bangladesh','Canada'];
+var desh = ['Pakistan','Afghanistan','Kuwait','Turkey','Palestine'];
+
+//uncomment and use any of these
+
+// console.log(arr)
+// console.log(arr.length)
+
+//Join two arrays
+//console.log(arr.concat(desh))
+
+// Joining elements of array
+// console.log(arr.join('-'))
+
+// Slicing part 
+// console.log(arr.slice(2,5))
+
+//first index of element
+// console.log(arr.indexOf('Canada'))
+
+//last index of element
+// console.log(arr.lastIndexOf('Canada'))
+
+// for each in array
+// let emparr = []
+
+// desh.forEach(function(item){
+//     emparr.push(item)
+// })
+// console.log(emparr)
+
+// array filter()
+// var ages = [23,21,13,13,11,13,09,99];
+// console.log(ages.filter(checkage))
+
+// function checkage(age){
+//     return age>18;
+// }
+
+//
+// console.log(arr[0].length)
+
+//push in array js
+// console.log(arr.push("nikhil"))
+// console.log(arr)
+// console.log(arr.pop())
+// console.log(arr)
+
+// console.log(arr.shift())
+// console.log(arr)
+// console.log(arr.unshift("India"))
+// console.log(arr)
+
+// console.log(arr.includes('India'))
